e05aef62fb106788e69e0a4893b8e6e40c453837
wlanhage <wlanhage@gmail.com>
2023-11-17 18:11:56 +0100

    added pictures and toggle for darkmode

--- script.js
+++ script.js
@@ -4,3 +4,15 @@
 document.getElementById('darkModeToggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
+
+
+document.querySelector('.toggle-button').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    const slider = document.querySelector('.toggle-slider');
+
+    if (slider.style.left === '7px' || slider.style.left === '') {
+        slider.style.left = '31px'; // Move slider to the right
+    } else {
+        slider.style.left = '7px'; // Move slider back to the left
+    }
+});
