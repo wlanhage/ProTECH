7275f359fa5356cb97fb3d84b13139ebf0c288a3
selmarosine <selma.gronsten@hotmail.se>
2023-11-26 01:33:20 +0100

    la till pilar till karusellen

--- slides.js
+++ slides.js
@@ -1,4 +1,3 @@
-/* KARUSELL */
 document.querySelectorAll(".carousel").forEach((carousel) => {
   const items = carousel.querySelectorAll(".carousel-item");
   const buttonsHtml = Array.from(items, () => {
@@ -8,23 +7,42 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
   carousel.insertAdjacentHTML(
     "beforeend",
     `<div class="carousel-nav">
-                ${buttonsHtml.join("")}
-            </div>`
+      ${buttonsHtml.join("")}
+    </div>
+    <span class="carousel-arrow carousel-arrow-left">&#8249;</span>
+    <span class="carousel-arrow carousel-arrow-right">&#8250;</span>`
   );
 
   const buttons = carousel.querySelectorAll(".carousel-button");
+  const arrows = carousel.querySelectorAll(".carousel-arrow");
+  let currentIndex = 0;
+
+  arrows.forEach((arrow) => {
+    arrow.addEventListener("click", () => {
+      if (arrow.classList.contains("carousel-arrow-left")) {
+        currentIndex = (currentIndex - 1 + items.length) % items.length;
+      } else {
+        currentIndex = (currentIndex + 1) % items.length;
+      }
+
+      updateCarousel();
+    });
+  });
 
   buttons.forEach((button, i) => {
     button.addEventListener("click", () => {
-      items.forEach((item) => item.classList.remove("carousel-item--selected"));
-      buttons.forEach((button) =>
-        button.classList.remove("carousel-button--selected")
-      );
-
-      items[i].classList.add("carousel-item--selected");
-      button.classList.add("carousel-button--selected");
+      currentIndex = i;
+      updateCarousel();
     });
   });
+
+  function updateCarousel() {
+    items.forEach((item) => item.classList.remove("carousel-item--selected"));
+    buttons.forEach((button) => button.classList.remove("carousel-button--selected"));
+
+    items[currentIndex].classList.add("carousel-item--selected");
+    buttons[currentIndex].classList.add("carousel-button--selected");
+  }
 
   items[0].classList.add("carousel-item--selected");
   buttons[0].classList.add("carousel-button--selected");
